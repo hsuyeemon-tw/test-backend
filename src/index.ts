bcd25061d29d857238a4016492d4e8f06f47396d
hsuyeemon <hsuyeemon.1@gmail.com>
2023-11-28 17:23:06 +0630

    mongodb setup

--- src/index.ts
+++ src/index.ts
@@ -4,7 +4,7 @@ import bodyParser from 'body-parser';
 import cookieParser from 'cookie-parser';
 import compression from 'compression';
 import cors from 'cors';
-
+import mongoose from 'mongoose';
 
 const app = express();
 
@@ -21,4 +21,10 @@ const server = http.createServer(app);
 
 server.listen(8080, ()=> {
     console.log('Server running on http://localhost:8080')
-})
+});
+
+const MONGO_URL = 'mongodb+srv://hsu:root@cluster0.zpkyxi7.mongodb.net/?retryWrites=true&w=majority';
+
+mongoose.Promise = Promise;
+mongoose.connect(MONGO_URL);
+mongoose.connection.on('error', (error:Error)=> console.log(error));
