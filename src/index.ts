fd57a30a9cc7becd10edd3f93866ee12a772484a
hsuyeemon <hsuyeemon.1@gmail.com>
2023-12-04 22:07:09 +0630

    authentication routing and login

--- src/index.ts
+++ src/index.ts
@@ -6,6 +6,8 @@ import compression from 'compression';
 import cors from 'cors';
 import mongoose from 'mongoose';
 
+import router from './router';
+
 const app = express();
 
 app.use(cors({
@@ -28,3 +30,5 @@ const MONGO_URL = 'mongodb+srv://hsu:root@cluster0.zpkyxi7.mongodb.net/?retryWri
 mongoose.Promise = Promise;
 mongoose.connect(MONGO_URL);
 mongoose.connection.on('error', (error:Error)=> console.log(error));
+
+app.use('/', router);
---
+++ src/router/authentication.ts
@@ -0,0 +1,8 @@
+import express from 'express';
+import {register} from '../controllers/authentication';
+
+const router = express.Router();
+
+export default (router:express.Router) => {
+    router.post('/auth/register',register);
+};
